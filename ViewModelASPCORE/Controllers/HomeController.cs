9d3c2802819c6c42d820dacac8d9e4f4f0c1a2e3
abhishek kumar <abhisingh2084@gmail.com>
2024-06-03 11:07:45 +0530

    Updates

--- ViewModelASPCORE/Controllers/HomeController.cs
+++ ViewModelASPCORE/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using ViewModelASPCORE.Models;
-using ViewModelASPCORE.Views;
+ 
 
 
 
---
+++ ViewModelASPCORE/Models/SchoolViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewModelASPCORE.Models
+{
+    public class SchoolViewModel
+    {
+        public List<Student> myStudents { get; set; }
+        public List<Teacher> myTeachers { get; set; }
+
+    }
+}
